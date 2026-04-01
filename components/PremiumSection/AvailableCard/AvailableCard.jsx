83e2715d9fec72b35c66f19b726221b10f37ab5d
Fahad01012002 <fahad01012002@gmail.com>
2026-04-01 23:13:40 +0600

    add card section and add functionality

--- components/PremiumSection/AvailableCard/AvailableCard.jsx
+++ components/PremiumSection/AvailableCard/AvailableCard.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import Card from './Card';
+
+const cardData = async () => {
+    const res = await fetch('/Data.json')
+    return res.json();
+}
 
 const AvailableCard = () => {
+    const cardsData = cardData();
     return (
-        <div>
-            
-        </div>
+            < div >
+            <Suspense fallback={<span className="loading loading-bars loading-xl"></span>}>
+                <Card cardsData={cardsData}></Card>
+            </Suspense>
+        </div >
     );
 };
 
